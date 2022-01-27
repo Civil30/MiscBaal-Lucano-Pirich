34fe186e989efa08e4056cd0017081c892500333
Civil30 <carly_arteaga@hotmail.com>
2022-01-26 23:19:24 -0300

    fuente y más detalles de producto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
--- src/products/products.js
+++ src/products/products.js
@@ -21,7 +21,7 @@ export const products = [
         title: "Peluche Squirtle",
         price: 3000,
         pictureUrl: squirtle_original,
-        detail: ""
+        detail: "¡Lleno de encanto aventurero, este Squirtle Poke Peluche está listo para emprender un viaje!. Un caparazón de felpa y una cola rizada, rematan un estilo divertido con bordados y tela suave. ¿Adónde irás con Squirtle?."
     },
     {
         id: 3,
@@ -29,7 +29,7 @@ export const products = [
         title: "Peluche Charmander",
         price: 3000,
         pictureUrl: charmander_original,
-        detail: ""
+        detail: "¡Lleno de encanto aventurero, este Charmander Poke Peluche está listo para emprender un viaje!. Una llama de cola brillante y esponjosa, completa un aspecto divertido con bordados y tela suave. ¿Adónde irás con Charmander?."
     },
     {
         id: 4,
@@ -37,7 +37,7 @@ export const products = [
         title: "Peluche Eevee",
         price: 3000,
         pictureUrl: eevee_original,
-        detail: ""
+        detail: "Este feliz peluche de Eevee está listo para hacerte compañía en tu próximo torneo de TCG, en el cine o en cualquier otro lugar. ¡Es un alegre compañero para todas tus aventuras Pokémon!."
     },
     {
         id: 5,
@@ -45,7 +45,7 @@ export const products = [
         title: "Peluche Wooloo",
         price: 3000,
         pictureUrl: wooloo_original,
-        detail: ""
+        detail: "Supersuave y esponjoso, este blandito Wooloo tiene ojos cuidadosamente bordados y un vellón de lana irresistiblemente tierno!. Es relajante para sostener, divertido para rodar y es una gran adición a tu colección de peluches."
     },
     {
         id: 6,
@@ -53,6 +53,6 @@ export const products = [
         title: "Peluche Froakie",
         price: 3000,
         pictureUrl: froakie_original,
-        detail: ""
+        detail: "¡Lleno de encanto aventurero, este Froakie Poke Peluche está listo para emprender un viaje!. Pequeña rana adorable de grandes ojos amarillos, completan un aspecto divertido con bordados y tela suave. ¿Adónde irás con Froakie?."
     }
 ]
